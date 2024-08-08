58dd1db37af90eade64e8c5b089b1f36c45a3652
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-08 15:49:10 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,27 @@
+// Console log a welcome message with instructions to mad libs
+
+
+
+// Prompt the user to enter parts of speech for mad libs
+// You should be asking for at least 4 words
+let adjective =prompt("Give me an adjective")
+let pluralnoun =prompt("Give me an pluralnoun")
+let verb =prompt("Give me an verb")
+let adjective2 =prompt("Give me an adjective2")
+// Create an array that stores the values the user entered
+let newarray=[]
+newarray.push(adjective)
+newaraay.push(pluralnoun)
+newarray.push(verb)
+newarray.push(adjective2)
+// Console log the story with the array values passed into the blanks
+// Make sure you are referencing the values of the array (rather than the user inputs themselves)
+// You can use any type of string manipulation that feels more intuitive
+console.log("In a small"$(newarray[0]),  "school in the Bronx, there was a problem with too many These" $(newarray[1]),"loved to "$(newarray[2]), "which caused Mr. P so many headaches. However, no one wanted to remove them because they were"$(newarray[3]) ,"so they stayed there forever")
+
+
+
+
+
+
+
